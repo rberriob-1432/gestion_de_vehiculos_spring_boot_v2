9326d94e4490db60df1422d4dc7e75910857e7d6
rberriob-1432 <rberriob@unicartagena.edu.co>
2026-09-15 18:04:30 -0500

    feat: add Spring Boot main application

---
+++ src/main/java/com/gestion_vehiculos/GestionVehiculosApplication.java
@@ -0,0 +1,12 @@
+package com.gestion_vehiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionVehiculosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionVehiculosApplication.class, args);
+    }
+}
